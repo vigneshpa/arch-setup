289cb49f69df11bc6a3bf497c8fd6e241286746e
vignesh <vignesh-p@outlook.com>
2021-07-07 11:48:54 +0530

    Initial Script

---
+++ prompt.sh
@@ -0,0 +1,42 @@
+#!/bin/sh -x
+
+set -eo pipefail
+
+# Arch linux  packages
+
+depackages=( zsh starship zsh-syntax-highlighting zsh-autosuggestions fortune-mod )
+depackages="${depackages[@]}"
+sudo pacman -S $depackages
+
+sudo usermod -s /bin/zsh $(whoami)
+
+dot_files_dir="$HOME/.config/zsh"
+mkdir -r $dot_files_dir
+
+echo "export ZDOTDIR=~/.config/zsh" > ~/.zshenv
+
+cat > "$dot_files_dir/.zshrc" <<- EOM
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+
+# Lines configured by zsh-newuser-install
+HISTFILE=./.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename './.zshrc'
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+
+
+# Custom Script
+alias ls='ls --color=auto'
+eval "$(starship init zsh)"
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+fortune
+EOM

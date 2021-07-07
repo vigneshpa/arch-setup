b0ac2ddc8ac554f204efb81b55ea99696705dbf3
vignesh <vignesh-p@outlook.com>
2021-07-07 20:16:57 +0530

    moving all files into $ZDOTDIR

--- prompt.sh
+++ prompt.sh
@@ -21,13 +21,13 @@ cat > "$dot_files_dir/.zshrc" <<- EOM
 
 
 # Lines configured by zsh-newuser-install
-HISTFILE=./.histfile
+HISTFILE="\$ZDOTDIR/.histfile"
 HISTSIZE=1000
 SAVEHIST=1000
 bindkey -e
 # End of lines configured by zsh-newuser-install
 # The following lines were added by compinstall
-zstyle :compinstall filename './.zshrc'
+zstyle :compinstall filename "\$ZDOTDIR/.zshrc"
 autoload -Uz compinit
 compinit
 # End of lines added by compinstall

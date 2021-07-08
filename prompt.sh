be5392d3ba801080f00df519920f0379f43cd4d9
vignesh <vignesh-p@outlook.com>
2021-07-08 20:11:50 +0530

    testing existance of pacman with better approch

--- prompt.sh
+++ prompt.sh
@@ -13,11 +13,10 @@ mkdir -p "$dot_files_dir/cache"
 packages="zsh starship fortune-mod"
 plugins="zsh-autosuggestions zsh-syntax-highlighting"
 
-pacman -V
-if [ $? -eq 0 ]
+if ! command -v pacman &> /dev/null
 then
     packages="$depackages $plugins"
-    sudo pacman -S $packages --noconfirm
+    sudo pacman -S "$packages" --noconfirm
 else
     echo "Warning:Pacman does not exists;unknown environment"
     plugins_dir="$dot_files_dir/plugins"
@@ -27,7 +26,7 @@ else
 for plu in "${plugins[@]}"
 do
     plug_dir="./plugins/\$plu"
-    mkdir -p \$plug_dir
+    mkdir -p "\$plug_dir"
     curl --proto '=https' --tlsv1.2 -sSf "https://raw.githubusercontent.com/zsh-users/\$plu/master/\$plu.zsh" -o "\$plugin_dir/\$plu.zsh"
 done
 EOL
@@ -36,8 +35,8 @@ fi
 
 cat > "$HOME/.zshenv" << EOL
 export PATH="\$PATH"            # Change this line to change path variable
-export ZDOTDIR=$dot_files_dir   # Required to load zshrc
-export ZPLUGDIR_X=$plugins_dir  # Required to load zsh plugins
+export ZDOTDIR="$dot_files_dir"   # Required to load zshrc
+export ZPLUGDIR_X="$plugins_dir"  # Required to load zsh plugins
 EOL
 
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/vigneshpa/arch-setup/main/.zshrc -o "$dot_files_dir/.zshrc"

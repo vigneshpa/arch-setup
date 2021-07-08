417ae93d4815297e53539e8849a0f759bbac9184
vignesh <vignesh-p@outlook.com>
2021-07-08 15:11:30 +0530

    creating a directory for cache files

--- prompt.sh
+++ prompt.sh
@@ -6,23 +6,27 @@ set -eo pipefail
 
 depackages=( zsh starship zsh-syntax-highlighting zsh-autosuggestions fortune-mod )
 depackages="${depackages[@]}"
-sudo pacman -S $depackages --noconfirm
-
+if[ $1 -eq '--no-pacman']
+then 
+  echo "Please install $depackages manually"
+else
+ sudo pacman -S $depackages --noconfirm
+fi
 sudo usermod -s /bin/zsh $(whoami)
 
 dot_files_dir="$HOME/.config/zsh"
-mkdir -p $dot_files_dir
+mkdir -p $dot_files_dir/cache
 
 echo "export ZDOTDIR=~/.config/zsh" > ~/.zshenv
 
 cat > "$dot_files_dir/.zshrc" <<- EOM
-#!/usr/bin/env zsh
+#!/zsh
 
 # If not running interactively, don't do anything
 [[ \$- != *i* ]] && return
 
 # History
-HISTFILE="\$ZDOTDIR/.histfile"
+HISTFILE="\$ZDOTDIR/cache/.histfile"
 HISTSIZE=1000
 SAVEHIST=1000
 
@@ -30,9 +34,9 @@ SAVEHIST=1000
 autoload -Uz compinit
 zstyle ':completion:*' menu select
 #case insensitive completion
-#zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zmodload zsh/complist
-compinit
+compinit -d "\$ZDOTDIR/cache/.zcompdump"
 _comp_options+=(globdots)
 
 # Disabling beep
@@ -40,7 +44,7 @@ unsetopt BEEP
 
 # Aliases
 alias ls='ls --color=auto'
-
+alias zshrc='nano ~/.config/zsh/.zshrc'
 # Starship
 eval "\$(starship init zsh)"
 
@@ -56,4 +60,5 @@ source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # Launching fortune
 fortune
+
 EOM

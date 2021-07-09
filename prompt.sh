87edede0e778f10f35310c61045a8a9627a224c4
vignesh <vignesh-p@outlook.com>
2021-07-09 16:21:59 +0530

    improving xhsh

--- prompt.sh
+++ prompt.sh
@@ -53,16 +53,10 @@ EOL
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/vigneshpa/arch-setup/main/.zshrc -o "$dot_files_dir/.zshrc"
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/vigneshpa/arch-setup/main/starship.toml -o "$dot_files_dir/../starship.toml"
 
-zsh_loc=$(which zsh)
-if [[ $? -eq 0]]
+if ! command -v pacman &> /dev/null
 then
-    chsh -s $zsh_loc
-    if[[ $? -eq 0 ]]
-    then
-        echo "Login again to see the changes"
-    else
-        echo "chsh command failed"
-    fi
+    chsh -s $(which zsh)
+    echo "Login again to see the changes"
 else
     echo "cannot change find zsh change the shell manually"
 fi
